69cf6ad8ba4c5c205ecd61e96a4aff8795b3308a
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-01 13:51:28 -0600

    added about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
-//import Hero from '../components/Hero';
+import Hero from '../components/Hero';
+import Content from '../components/Content'
 
 function AboutPage(props) {
     return(
-        <p>About Page Works!</p>
-    )
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>a bunch of wordy words</p>
+            </Content>
+        </div>
+    );
     
 }
 
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,45 @@
 import React from 'react';
-//import Hero from '../components/Hero';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+import { Form } from 'react-bootstrap'
 
 
-function ContactPage(props) {
-    return(
-        <p>Contact Page Works</p>
-    )
+class ContactPage extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            disabled: '',
+            emailSent: null
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                <Hero title={this.props.title} />
+
+                <Content>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label htmlFor="Full Name">Full Name</Form.Label>
+                            <Form.Control id='full-name' name='name' type='text' value={this.state.name} onChange={this.handleChange}/>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label htmlFor="Full Name">Full Name</Form.Label>
+                            <Form.Control id='full-name' name='name' type='text' value={this.state.name} onChange={this.handleChange}/>
+                        </Form.Group>
+                    </Form>
+
+
+                </Content>
+            </div>
+        )
+    }
 }
 
 export default ContactPage;
