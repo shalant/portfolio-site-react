da4cf713853e9347dff7f5328b3d630a5ecf793b
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-01 22:50:16 -0600

    added sendspace

---
+++ portfolio-api/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendGrid/mail');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/api', (req, res, next) => {
+    res.send('API Status: I\'m awesome')
+});
+
+app.listen(3030, '0.0.0.0');
--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
-
 class App extends React.Component {
 
   constructor(props) {
